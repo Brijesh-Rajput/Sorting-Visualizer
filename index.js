b11f281d731c4e2631921f653fecbf1199898773
Brijesh Rajput <96364664+Brijesh-Rajput@users.noreply.github.com>
2023-07-24 20:18:13 +0530

    Insertion & Selection sort visualizer implemented!
    Signed-off-by: Brijesh Rajput <96364664+Brijesh-Rajput@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const container = document.querySelector(".container");
-const Sortedcontainer = document.querySelector(".Sortedcontainer");
+// const sortedContainer = document.querySelector(".sortedContainer");
+
+// const sortingAlgorithmName = document.querySelector("#sortingAlgorithmNameSection");
+
 // getting sorting Algo name which is selected in select option
 // const sortingAlgorithm = document.querySelector("#selectSortingAlgo");
 // getting array elements from user
 // const arr  = document.querySelector('#array');
 
 class SortingVisualizer {
+	static #setBarElement = function (height, color) {
+		return `<div class="bar" style="height:${
+			height
+			// * 10
+		}px; background-color:${color};"></div>`;
+	};
+
 	static #wait = function (seconds) {
 		return new Promise(function (resolve) {
 			setTimeout(resolve, seconds * 1000);
@@ -20,20 +30,11 @@ class SortingVisualizer {
 
 		for (const [indx, element] of arr.entries()) {
 			if (indx === indx1) {
-				barHtmlElement = `<div class="bar" style="height:${
-					element
-					// * 10
-				}px; background-color:${color1};"></div>`;
+				barHtmlElement = this.#setBarElement(element, color1);
 			} else if (indx === indx2) {
-				barHtmlElement = `<div class="bar" style="height:${
-					element
-					// * 10
-				}px; background-color:${color2};"></div>`;
+				barHtmlElement = this.#setBarElement(element, color2);
 			} else {
-				barHtmlElement = `<div class="bar" style="height:${
-					element
-					// * 10
-				}px; background-color:violet;"></div>`;
+				barHtmlElement = this.#setBarElement(element, "violet");
 			}
 			container.insertAdjacentHTML("beforeend", barHtmlElement);
 		}
@@ -81,6 +82,22 @@ class SortingVisualizer {
 		}
 	};
 
+	static async #doVisualize(tempArrayForSorting, swapMovements, seconds) {
+		this.#show(tempArrayForSorting);
+
+		await this.#wait(seconds);
+
+		await this.#startVisualizer(
+			tempArrayForSorting,
+			swapMovements,
+			seconds
+		);
+
+		// No need to again call wait() bcoz: At last it startVisualizer() wait() is already called when loops comes to the end of the element.
+
+		this.#show(tempArrayForSorting);
+	}
+
 	static async bubbleSort(arr, seconds) {
 		const length = arr.length;
 		const tempArrayForSorting = arr.slice();
@@ -97,25 +114,63 @@ class SortingVisualizer {
 		}
 		// console.log(swapMovements, swapMovements.length);
 
-		// ============ Main Logic ==========
-		// array display before starting its visualization
-		this.#show(tempArrayForSorting); // original array will be passed.
+		// // ============ Main Logic ==========
+		// // array display before starting its visualization
+		// this.#show(tempArrayForSorting); // original array will be passed.
 
-		await this.#wait(seconds);
-		// visualization is started --> This code should run synchronous but we have to pause at swapping time and Not that on that time no other code will execute.
-		await this.#startVisualizer(
-			tempArrayForSorting,
-			swapMovements,
-			seconds
-		);
+		// await this.#wait(seconds);
+		// // visualization is started --> This code should run synchronous but we have to pause at swapping time and Not that on that time no other code will execute.
+		// await this.#startVisualizer(
+		// 	tempArrayForSorting,
+		// 	swapMovements,
+		// 	seconds
+		// );
 
-		// // array display after swapping its visualization
-		this.#show(tempArrayForSorting); // final array will be passed. BUT how ?
+		// // // array display after swapping its visualization
+		// this.#show(tempArrayForSorting); // final array will be passed. BUT how ?
+
+		await this.#doVisualize(tempArrayForSorting, swapMovements, seconds);
 	}
 
-	static selectionSort(arr) {}
+	static async selectionSort(arr, seconds) {
+		const length = arr.length;
+		const tempArrayForSorting = arr.slice();
+		let swapMovements = [];
 
-	static insertionSort(arr) {}
+		// For storing positions of swapping ===> Applying selection sort
+		for (let i = 0; i < length - 1; i++) {
+			for (let j = i + 1; j < length; j++) {
+				if (arr[i] > arr[j]) {
+					//swapp
+					swapMovements.push([i, j]);
+					[arr[i], arr[j]] = [arr[j], arr[i]];
+				}
+			}
+		}
+
+		await this.#doVisualize(tempArrayForSorting, swapMovements, seconds);
+	}
+
+	static async insertionSort(arr, seconds) {
+		const length = arr.length;
+		const tempArrayForSorting = arr.slice();
+		let swapMovements = [];
+
+		// For storing Positions of swapping ===> Implementing insertion sorting
+		for (let i = 1; i < length; i++) {
+			// i started from index bcoz: 1st element of a array is alreday sorted
+			for (let j = i - 1; j >= 0; j--) {
+				if (arr[i] < arr[j]) {
+					// swapp elements
+					[arr[i], arr[j]] = [arr[j], arr[i]];
+					swapMovements.push([i, j]);
+					i = j;
+				}
+			}
+		}
+
+		await this.#doVisualize(tempArrayForSorting, swapMovements, seconds);
+	}
 
 	static quickSort(arr) {}
 
@@ -141,19 +196,30 @@ for (let i = 1; i < 40; i++) {
 console.log(arr);
 
 const seconds = 0; // Hardcoded data
-const sortingAlgorithm = "bubbleSort"; // Hardcoded data
+// const sortingAlgorithm = "bubbleSort"; // Hardcoded data
+const sortingAlgorithm = "insertionSort"; // Hardcoded data
+// const sortingAlgorithm = "selectionSort"; // Hardcoded data
+
 // Before continue check whether it contains only integer values or not ? are they in given defined range or not ?
 
 switch (sortingAlgorithm) {
 	case "bubbleSort":
 		console.log("Bubble sort Algorithm started");
+
+		// setting sorting name to the display as per the user selection
+		// sortingAlgorithmName.innerText = sortingAlgorithm;
+
 		SortingVisualizer.bubbleSort(arr, seconds);
+		// (async function(){ await SortingVisualizer.bubbleSort(arr, seconds); });
 		break;
+
 	case "selectionSort":
-		SortingVisualizer.selectionSort(arr);
+		console.log("Selection sort Algorithm started");
+		SortingVisualizer.selectionSort(arr, seconds);
 		break;
 	case "insertionSort":
-		SortingVisualizer.insertionSort(arr);
+		console.log("Insertion sort Algorithm started");
+		SortingVisualizer.insertionSort(arr, seconds);
 		break;
 	case "quickSort":
 		SortingVisualizer.quickSort(arr);
@@ -166,9 +232,13 @@ switch (sortingAlgorithm) {
 }
 
 // -------------- Next Project -----------------
-// DOn't know why it becomes slow in between execution.
+// DOn't know why it becomes slow in between execution. Does it is due to asynchronous code running ?
 // We can change the colour of swap elements after each swapping of bar-elements.
 // Now, instead of storing moves do directly swapping.
+// set sorting algorithm name dynamically on the display as user selects the sorting Algorithm.
+// Implement also for decreasing order also
+// Implement slowly all sorting algorithm
+// How polymorphism is implemeneted in Javascript ?
 
 // Linear Search and Binary Search Visualizer and Ternary Search
 // N Queen
